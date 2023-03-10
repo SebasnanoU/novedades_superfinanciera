1bf55e4bca3abd731d681030e6291616ee461e1a
Sebstian Rodriguez <jsrodriguezp03@libertadores.edu.co>
2023-03-10 13:24:57 -0500

    Deploy fly.oi

--- api_super/settings.py
+++ api_super/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6a1u(q3ej9sexqq46yk47bfw=x8yh%x&7gl#e^-maplg$1)$z!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
